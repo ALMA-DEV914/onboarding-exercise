d37800087bc5b06341bf3aa3870d826f155e827e
ALMA_DEV914 <aungonalna58@gamil.com>
2022-09-30 02:39:50 -0700

    add filter search input

--- src/components/MovieLists.js
+++ src/components/MovieLists.js
@@ -7,8 +7,7 @@ export default function MovieLists() {
   const [data, setData] = useState([]);
   const [count, setCount] = useState(0);
   const [count1, setCount1] = useState(0);
-  const [buttons, setButtons] = useState([])
-  
+
   const getData = () => {
     fetch("movies.json", {
       headers: {
@@ -46,61 +45,79 @@ export default function MovieLists() {
 
   const handleIncrement = () => {
     setCount(count + 1);
-    setButtons()
-}
+  };
   const handleDecrement = () => {
-    setCount1(count1 - 1) 
-  }
-  
+    setCount1(count1 - 1);
+  };
 
   return (
     <div className="search-wrapper">
       <Input
-      className="input"
+        className="input"
         icon="search"
-        placeholder="Search..."
+        placeholder="Search by title..."
         onChange={(e) => searchItems(e.target.value)}
       />
-     <div className="container">
-      <Card.Group className="row">
-        {searchInput.length > 1
-          ? filteredResults.map((item) => {
-              return (
-                
-                <Card>
-                  <Card.Content>
-                    <Card.Header><img src={item.url} alt="item.title" /></Card.Header>
-                    <Card.Description>
-                     <h3>{item.title}</h3>
-                    </Card.Description>
-                  
-                  <button onClick={handleIncrement}>Likes: {item.likes + count}</button>
-                    <button  onClick={handleDecrement}>Dislikes: {item.dislikes + count1}</button>
-                  </Card.Content>
-                </Card>
-              );
-            })
-          : data &&
-            data.length > 0 &&
-            data.map((item, index) => {
-              return (
-                <Card key={index} id={`${item.id}`}>
-                  <Card.Content>
-                    <Card.Header>
-                      {" "}
-                      <img src={item.url} alt="item.title" />
-                    </Card.Header>
-                    <Card.Description>
-                      <h3>{item.title}</h3>
-                    </Card.Description>
-                   <button key={index} onClick={handleIncrement}>Likes:{item.likes + count}</button> {" "}
-                    <button  onClick={handleDecrement}>Dislikes: {item.dislikes + count1}</button>
-                  </Card.Content>
-                </Card>
-              );
-            })}
-      </Card.Group>
-    </div>
+      <div className="container">
+        <Card.Group className="row">
+          {searchInput.length > 1
+            ? filteredResults.map((item) => {
+                return (
+                  <Card>
+                    <Card.Content>
+                      <Card.Header>
+                        <img src={item.url} alt="item.title" />
+                      </Card.Header>
+                      <Card.Description>
+                        <h3>
+                          {item.title}
+                          <br></br>
+                          <span>{item.category}</span>
+                        </h3>
+                        <button onClick={handleDecrement}>
+                          Dislikes: <i className="fa fa-thumbs-down"></i>
+                          {item.dislikes + count1}
+                        </button>
+                        <button onClick={handleIncrement}>
+                          Likes:<i className="fa fa-thumbs-up"></i>{" "}
+                          {item.likes + count}
+                        </button>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                );
+              })
+            : data &&
+              data.length > 0 &&
+              data.map((item, index) => {
+                return (
+                  <Card key={index}>
+                    <Card.Content>
+                      <Card.Header>
+                        {" "}
+                        <img src={item.url} alt="item.title" />
+                      </Card.Header>
+                      <Card.Description>
+                        <h3>
+                          {item.title}
+                          <br></br>
+                          <span>{item.category}</span>
+                        </h3>
+                        <button onClick={handleDecrement}>
+                          Dislikes:<i className="fa fa-thumbs-down"></i>{" "}
+                          {item.dislikes + count1}
+                        </button>
+                        <button onClick={handleIncrement}>
+                          Likes:<i className="fa fa-thumbs-up"></i>{" "}
+                          {item.likes + count}
+                        </button>{" "}
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                );
+              })}
+        </Card.Group>
+      </div>
     </div>
   );
-}      
+}
