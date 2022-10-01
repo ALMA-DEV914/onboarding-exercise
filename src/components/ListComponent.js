27c71a5d617554d4f38913faf6d89e7b65017335
ALMA_DEV914 <aungonalna58@gamil.com>
2022-10-01 12:25:53 -0700

    enhance style of button

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -4,11 +4,12 @@ import { Card } from "semantic-ui-react";
 function SearchResults({ item }) {
   const [count, setCount] = useState(0);
   const [count1, setCount1] = useState(0);
+
   const handleIncrement = () => {
     setCount(count + 1);
   };
   const handleDecrement = () => {
-    setCount1(count1 + 1);
+    setCount1(count1 - 1);
   };
   return (
     <Card>
@@ -23,11 +24,12 @@ function SearchResults({ item }) {
             <span>{item.category}</span>
           </h3>
           <button onClick={handleDecrement}>
-            Dislikes: <i className="fa fa-thumbs-down"></i>
+            Dislikes:<i className="fa fa-thumbs-down"></i>{" "}
             {item.dislikes + count1}
           </button>
           <button onClick={handleIncrement}>
-            Likes:<i className="fa fa-thumbs-up"></i> {item.likes + count}
+            Likes:<i className="fa fa-thumbs-up"></i>{" "} 
+            {item.likes + count}
           </button>
         </Card.Description>
       </Card.Content>
